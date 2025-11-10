a62c43cc16433b869b358aff4728c8653153f2a1
ElioSpena <elio.spena.94@gmail.com>
2025-11-10 16:07:15 +0100

    feat: creata lista di titoli articoli

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ function App() {
 
   return (
     <>
-    <h1>Prova</h1>
+    <header className="text-center py-4">
+      <h1>Blog</h1>
+    </header>
+
+    <main>
+
+      <section className="container">
+
+            <ul>
+
+              {data.map(curData=> (
+                <li className="list-group">
+                  <h3 className="py-4">{curData.titolo}</h3>
+                 {/*  <p>{curData.contenuto}</p> */}
+                </li>
+              ))}
+
+            </ul>
+
+
+      </section>
+
+    </main>
+   
     </>
   )
 }
