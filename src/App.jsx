3448bfad2fd82f4c0f3c94f103357caf444f0f69
ElioSpena <elio.spena.94@gmail.com>
2025-11-11 15:53:30 +0100

    feat: aggiunto form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import data from "./assets/data/data.js"
+import { useState } from "react"
+import  musicBlogArticles from "./assets/data/data.js"
 
 
 function App() {
+  const [newArticle, setNewArticle] = useState("");
+  const [articles, setArticles] = useState(musicBlogArticles);
 
+
+  function submit(event) {
+    event.preventDefault();
+      const newArticleObject = {
+      id: [articles.length + 1],
+      titolo: newArticle, 
+      contenuto: "", 
+    };
+    setArticles((articles)=> [...articles, newArticleObject ]);
+    setNewArticle("");
+  }
+ 
+  
   return (
     <>
     <header className="text-center py-4">
@@ -12,11 +28,29 @@ function App() {
     <main>
 
       <section className="container">
+          
+          <form
+          onSubmit={submit}
+           className="text-center">
 
-            <ul>
+            <input 
+            value={newArticle}
+            onChange={(event) => {
+              setNewArticle(event.target.value);
+            }}
+            className="form-control mb-3"
+            type="text" />
 
-              {data.map(curData=> (
-                <li className="list-group">
+            <button
+            className="btn btn-primary mb-4">
+              Aggiungi</button>
+          </form>
+           
+          
+            <ul className="text-center">
+
+              {articles.map(curData=> (
+                <li key={curData.id} className="list-group">
                   <h3 className="py-4">{curData.titolo}</h3>
                  {/*  <p>{curData.contenuto}</p> */}
                 </li>
